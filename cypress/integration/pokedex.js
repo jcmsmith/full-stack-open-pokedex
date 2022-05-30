0282bbb6184cc2693c3e9a7a349ef0af20e44fbf
jcmsmith <13570256+jcmsmith@users.noreply.github.com>
2022-05-30 02:35:59 -0700

    Exercise 11.9

--- cypress/integration/pokedex.js
+++ cypress/integration/pokedex.js
@@ -6,4 +6,16 @@ describe('Pokedex', function () {
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
   })
+
+  it('specific pages can be opened', function () {
+    cy.visit('http://localhost:5000')
+    cy.get('a[href*="/pokemon/weedle"]').click()
+    cy.contains('weedle')
+    cy.contains('shield dust')
+
+    cy.visit('http://localhost:5000')
+    cy.get('a[href*="/pokemon/voltorb"]').click()
+    cy.contains('voltorb')
+    cy.contains('soundproof')
+  })
 })
