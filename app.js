44bb090a1cfe6510fcefb34be994aa47685e51fd
jcmsmith <13570256+jcmsmith@users.noreply.github.com>
2022-05-30 22:09:40 -0700

    Simulate application breakdown

--- app.js
+++ app.js
@@ -6,10 +6,6 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.get('/health', (req, res) => {
-  res.status(404)
-})
-
 app.get('/version', (req, res) => {
   res.send('3') // change this string to ensure a new version deployed
 })
